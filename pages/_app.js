3616dac7d904de9094706c882b7abd262ae3f3bd
s0ndip <s0ndip@outlook.com>
2024-12-20 16:15:46 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,14 @@
-import 'semantic-ui-css/semantic.min.css'; // Import Semantic UI CSS
-import '../styles/globals.css';  // Your custom global styles
+import '../styles/globals.css'; // Importing global styles
+import 'semantic-ui-css/semantic.min.css'; // Importing Semantic UI CSS for styling
+import { Container } from 'semantic-ui-react'; // Importing Semantic UI's Container component
+
+// Custom App component that wraps your pages with global configurations
+function MyApp({ Component, pageProps }) {
+  return (
+    <Container style={{ marginTop: '50px' }}>
+      <Component {...pageProps} />
+    </Container>
+  );
+}
+
+export default MyApp;
